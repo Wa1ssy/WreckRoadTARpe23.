83d71683083a027c68c5a576b7215217c3393aa3
opilane <opilane@ORDI-402604>
2024-10-17 11:59:09 +0300

    Cars now has an index page which currently is treated as the inventory for the player

---
+++ Wreck-Road/Controllers/CarsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wreck_Road.Controllers
+{
+    public class CarsController : Controller
+    {
+        /*
+         CarsController controls all functions for cars including Missions
+         */
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
