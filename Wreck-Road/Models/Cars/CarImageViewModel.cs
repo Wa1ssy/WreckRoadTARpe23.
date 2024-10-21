bb793e6afa73b5a1b7c9bfa117ef1f8269c88be8
opilane <opilane@HP-CZC321705R>
2024-10-21 16:31:04 +0300

    Created CarImageviewModel

---
+++ Wreck-Road/Models/Cars/CarImageViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Wreck_Road.Models.Cars
+{
+    public class CarImageViewModel
+    {
+        public Guid ImageID { get; set; }
+        public string ImageTitle { get; set; }
+        public byte[] ImageData { get; set; }
+        public string Image {  get; set; }
+        public Guid? CarID { get; set; }
+    }
+}
