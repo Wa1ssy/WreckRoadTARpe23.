4e96dad1bfebb6d79bef301bede3d840d2d834e0
opilane <opilane@HP-CZC321705F>
2024-11-08 13:20:41 +0200

    qdqdq2df

---
+++ WreckRoad/Models/Cars/CarDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace WreckRoad.Models.Cars
+{
+    public class CarDetailsViewModel
+    {
+        public Guid ID { get; set; }
+        public string CarName { get; set; }
+        public int CarXP { get; set; }
+        public int CarXPNextLevel { get; set; }
+        public int CarLevel { get; set; }
+        public CarType CarType { get; set; }
+        public CarStatus CarStatus { get; set; }
+        public int TurnSpeed { get; set; }
+        public string TurnName { get; set; }
+        //public List<IFormFile> Files { get; set; }
+        public List<CarImageViewModel> Image { get; set; } = new List<CarImageViewModel>();
+    }
+}
