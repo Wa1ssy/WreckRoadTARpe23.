706cae49cc5f158933cb4b907e5a0313c12cc634
opilane <opilane@HP-CZC321706B>
2024-11-20 15:20:38 +0200

    Added accounts interface with service aswell

---
+++ WreckRoad.ApplicationServices/Services/AccountsServices.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WreckRoad.Core.Domain;
+
+namespace WreckRoad.ApplicationServices.Services
+{
+    public class AccountsServices : IAccountServices
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountsServices
+            (
+                UserManager<ApplicationUser> userManager,
+                SignInManager<ApplicationUser> signInManager
+            )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+    }
+
+}
