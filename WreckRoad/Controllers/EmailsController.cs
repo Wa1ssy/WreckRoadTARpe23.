12f7bc26dfcc64a6b780d14dbe5ca6b2f51c74c2
opilane <opilane@HP-CZC321705F>
2024-12-04 15:43:22 +0200

    oifjwuivhnewjikbierujbjnmrt

---
+++ WreckRoad/Controllers/EmailsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WreckRoad.Core.Dto;
+using WreckRoad.Models.Emails;
+
+namespace WreckRoad.Controllers
+{
+    public class EmailsController : Controller
+    {
+        private readonly IEmailServices _emailServices;
+
+        public EmailsController(IEmailServices emailServices)
+        {
+            _emailServices = emailServices;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(EmailViewModel viewModel) 
+        {
+            var dto = new EmailDto()
+            {
+                To = viewModel.To,
+                Subject = viewModel.Subject,
+                Body = viewModel.Body,
+            };
+            _emailServices.SendEmail(dto);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
